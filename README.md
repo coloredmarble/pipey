1904389df17522e34dafc3a7689c41c77b0ff9df
gitretard <znoobbitcatspro@gmail.com>
2024-03-16 20:37:20 +0700

    stuff

--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 this is a relatively shitty clone of [`tap`](https://crates.io/crates/tap) but in 3 lines (for readability)
 
 made this because i am bored as fuck
+
+```
+tap = access the value temporarily. and return it
+pipe = access value and return whatever
+poof = like "pipe". but this marks the fact that this will return nothing. this allows chaining multiple statements
+```
+
+usage:
+
+```
+use pipey::Pipey;
+fn main(){
+    // the value is created
+    (10)
+    // this will allow temporary access to the value
+    .tap(|n| println!("value before adding 10 to it is {n}"))
+    // this will create another value. using the previous one
+    .pipe(|n| n + 10)
+    // the next function call will be the last to use this value!
+    .poof(|n| println!("the value is {n}"));
+    // the value is dropped. not leaving a mark, you will be the last to remember it, and then it will be gone again, forever
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 macro_rules! inline_all{($($fn:item)*)=>{$(#[inline(always)]$fn)*};}
-pub trait Pipey where Self: Sized {inline_all!{fn tap(self,f: impl FnOnce(&Self)) -> Self{f(&self); self} fn tap_mut(mut self,f: impl FnOnce(&mut Self)) -> Self{f(&mut self); self} fn pipe<T>(self, f: impl FnOnce(Self) -> T) -> T {f(self)} fn pipe_ref<T>(&self,f: impl FnOnce(&Self) -> T) -> T {f(self)} fn pipe_mut<T>(&mut self,f: impl FnOnce(&mut Self) -> T) -> T {f(self)}}}
-impl<T> Pipey for T{} // this fixes bounds
+pub trait Pipey where Self: Sized {inline_all!{fn tap(self,f: impl FnOnce(&Self)) -> Self{f(&self); self} fn tap_mut(mut self,f: impl FnOnce(&mut Self)) -> Self{f(&mut self); self} fn pipe<T>(self, f: impl FnOnce(Self) -> T) -> T {f(self)} fn pipe_ref<T>(&self,f: impl FnOnce(&Self) -> T) -> T {f(self)} fn pipe_mut<T>(&mut self,f: impl FnOnce(&mut Self) -> T) -> T {f(self)} fn poof(self,f: impl FnOnce(Self)) {f(self)}}}
+impl<T> Pipey for T {}
